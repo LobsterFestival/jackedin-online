8766ad0fe4513a43110416567bb307faca006e41
Jeremy Locatelli <jlocatelli@csumb.edu>
2017-09-27 00:04:16 +0000

    we in here

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     <head>
         <title> Testing </title>
         <meta charset="utf-8" />
+        
     </head>
-    <body>
-    TEST PUSH
+    <body id="i_body">
+    <marquee><h1>Jacked in... Online</h1></marquee>
     </body>
 </html>
